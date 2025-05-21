08f1a9cb98d9bc6677dc79387366c71ed3d6105c
LourdesFrrl <ferraiolimarialourdesfacultad@gmail.com>
2025-05-21 17:33:22 -0300

    agrege la funcion para borrar tarea

--- JS/agregarTarea.js
+++ JS/agregarTarea.js
@@ -3,12 +3,19 @@ const AniadirTarea =(e)=>{
     const inputTarea = document.getElementById("inputTarea").value
     const lista = document.getElementById("listaTareas")
     const li = document.createElement("li")
-    li.textContent = inputTarea /*le agrega el valor del input*/
+    li.innerHTML =`${inputTarea} <buttom class=btn type=button id="btnBorrar">❌</buttom>` /*le agrega el valor del input*/
     li.classList.add('list-group-item')/*le agrega el estilo*/
     lista.appendChild(li) /*le agrega un hijo al ul*/
     formularioTarea.reset();/*limpia el input*/
 }
 
+const borrar =()=>{
+    const item = document.getElementsByTagName("li")
+    item.remove()
+}
+
+const botonBorrar = document.getElementById("btnBorrar")
 const formularioTarea = document.getElementById("formularioTarea")
 
 formularioTarea.addEventListener("submit", AniadirTarea)
+botonBorrar.addEventListener("click",borrar)
